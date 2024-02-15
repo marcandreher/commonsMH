3ac57c88454debab630a1037644b4b1a434c7563
marcandreher <herpersmarcandre3@gmail.com>
2024-02-15 13:25:18 +0100

    Update SitemapGenerator to use GoogleCodeSitemapUrl.Options

--- src/main/java/commons/marcandreher/Commons/SitemapGenerator.java
+++ src/main/java/commons/marcandreher/Commons/SitemapGenerator.java
@@ -2,9 +2,11 @@ package commons.marcandreher.Commons;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
 
 import com.redfin.sitemapgenerator.WebSitemapGenerator;
-import com.redfin.sitemapgenerator.WebSitemapUrl;
+import com.redfin.sitemapgenerator.GoogleCodeSitemapUrl.Options;
 
 public class SitemapGenerator {
 
@@ -20,8 +22,16 @@ public class SitemapGenerator {
         wsg.addUrl(domain + url);
     }
 
-    public void addSitemapUrl(WebSitemapUrl url) {
-        wsg.addUrl(url);
+    public void addSitemapUrl(double priority, String url) {
+        try {
+            Options options = new Options(new URL(domain + url), "xml");
+            options.priority(priority);
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
+            options.lastMod(cal.getTime());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void write() {
