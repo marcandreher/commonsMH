7115a6ba904df2b6c5ed5a062fdb24346ff62213
marcandreher <herpersmarcandre3@gmail.com>
2024-02-22 15:29:40 +0100

    ref: meta

--- src/main/java/commons/marcandreher/Engine/Meta.java
+++ src/main/java/commons/marcandreher/Engine/Meta.java
@@ -5,66 +5,94 @@ public class Meta {
     public static String siteTitle = "CommonsMH";
     public static String domain = "http://localhost/";
 
-    public String robots = "index, follow";
-    public String openGraphType = "website";
-    public String twitterCardType = "summary";
+    private String robots = "index, follow";
+    private String openGraphType = "website";
+    private String twitterCardType = "summary";
 
-    public String canonial;
-    public String url;
 
-    public String image;
-    public String openGraphImage;
+    private String url;
 
-    public String description;
-    public String openGraphDescription;
-    public String twitterDescription;
+    private String image;
 
-    public String title;
-    public String openGraphTitle;
-    public String twitterTitle;
+    private String description;
 
-    public String[] keywords;
+    private String title;
+
+
+    private String[] keywords;
 
     public Meta(String image, String title, String description, String url, String... keywords) {
         this.image = image;
-        this.openGraphImage = image;
         this.description = description;
-        this.openGraphDescription = description;
-        this.twitterDescription = description;
         this.title = title;
-        this.openGraphTitle = title;
-        this.twitterTitle = title;
         this.url = url;
-        this.canonial = url;
         this.keywords = keywords;
+    }
+
+
+    public String getRobots() {
+        return this.robots;
+    }
+
+    public void setRobots(String robots) {
+        this.robots = robots;
+    }
+
+    public String getOpenGraphType() {
+        return this.openGraphType;
+    }
+
+    public void setOpenGraphType(String openGraphType) {
+        this.openGraphType = openGraphType;
+    }
+
+    public String getTwitterCardType() {
+        return this.twitterCardType;
+    }
+
+    public void setTwitterCardType(String twitterCardType) {
+        this.twitterCardType = twitterCardType;
+    }
+
+    public String getUrl() {
+        return domain + this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return this.image;
     }
 
     public void setImage(String image) {
         this.image = image;
-        this.openGraphImage = image;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return this.title + " | " + siteTitle;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String[] getKeywords() {
+        return this.keywords;
     }
 
     public void setKeywords(String... keywords) {
         this.keywords = keywords;
     }
-
-    public void setTitle(String title) {
-        title = title + " | " + siteTitle;
-        this.title = title;
-        this.twitterTitle = title;
-        this.openGraphTitle = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-        this.twitterDescription = description;
-        this.openGraphDescription = description;
-    }
-
-    public void setUrl(String url) {
-        url = domain + url;
-        this.url = url;
-        this.canonial = url;
-    }
+    
 
 }
