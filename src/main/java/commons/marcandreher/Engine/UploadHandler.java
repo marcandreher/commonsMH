1f057837920d82cbb638ec2d43ce7a3943875c46
marcandreher <herpersmarcandre3@gmail.com>
2024-02-24 14:48:33 +0100

    ref: upload handler

--- src/main/java/commons/marcandreher/Engine/UploadHandler.java
+++ src/main/java/commons/marcandreher/Engine/UploadHandler.java
@@ -1,9 +1,12 @@
 package commons.marcandreher.Engine;
 
 import javax.servlet.MultipartConfigElement;
- 
+import javax.servlet.http.HttpServletResponse;
+
 import commons.marcandreher.Commons.Database;
+import commons.marcandreher.Commons.Flogger;
 import commons.marcandreher.Commons.MySQL;
+import commons.marcandreher.Commons.Flogger.Prefix;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
@@ -28,21 +31,33 @@ public class UploadHandler implements Filter {
 
     @Override
     public void handle(Request request, Response response) throws Exception {
+        // Set the multipart configuration for the request
         request.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfig);
 
         MySQL mysql = null;
-        if (action != null) {
-            mysql = Database.getConnection();
-            action.beforeUpload(request, response, mysql);
-  
-        }
+        try {
+            if (action != null) {
+                // Get MySQL connection
+                mysql = Database.getConnection();
+                // Execute actions before upload
+                action.beforeUpload(request, response, mysql);
+            }
 
-       
-        if (action != null) {
-            action.afterUpload(request, response, mysql);
-            mysql.close();
+            // Action after upload
+            if (action != null) {
+                action.afterUpload(request, response, mysql);
+            }
+        } catch (Exception e) {
+        
+            Flogger.instance.log(Prefix.ERROR, e.getMessage(), 0);
+            e.printStackTrace();
+
+            response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.body("Internal Server Error");
+        } finally {
+            if (mysql != null) {
+                mysql.close();
+            }
         }
-       
     }
-
 }
