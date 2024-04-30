b9fc6d30f0aecfe2cb1c21c0e133ef628700ee39
marcandreher <herpersmarcandre3@gmail.com>
2024-04-30 23:39:12 +0200

    ref: good cmd engine noew?

--- src/main/java/commons/marcandreher/Input/CommandHandler.java
+++ src/main/java/commons/marcandreher/Input/CommandHandler.java
@@ -3,10 +3,13 @@ package commons.marcandreher.Input;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.UserInterruptException;
+import org.jline.reader.impl.history.DefaultHistory;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
@@ -19,54 +22,14 @@ import commons.marcandreher.Input.Commands.Shutdown;
 import commons.marcandreher.Input.Commands.TestRoute;
 import commons.marcandreher.Input.Commands.ThreadCheck;
 
-public class CommandHandler extends Thread {
-
+public class CommandHandler {
     private Flogger logger;
-
-    public CommandHandler(Flogger llogger) {
-        this.logger = llogger;
-
-    }
-
     public static List<Command> initializedCommands = new ArrayList<>();
+    private ExecutorService executorService;
 
-    public void run() {
-        Terminal terminal = null;
-        try {
-            terminal = TerminalBuilder.builder().system(true).build();
-        } catch (IOException e) {
-            Flogger.instance.error(e);
-            return;
-        }
-        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
-        logger.log(Prefix.INFO, "CommandHandler v1.9 | Type help", 1);
-        while (true) {
-            try {
-                String input = reader.readLine("admin@commonsmh:~$ ");
-
-                if (input == null)
-                    break;
-
-                String[] argsCmd = input.split(" ");
-
-                Boolean foundCmd = false;
-                for (Command cmd : initializedCommands) {
-                    if (argsCmd[0].equalsIgnoreCase(cmd.getName())) {
-                        cmd.executeAction(argsCmd, logger);
-                        foundCmd = true;
-                        break;
-                    }
-                }
-
-                if (!foundCmd) {
-                    logger.log(Prefix.INFO, "-> No Command found | Type help", 1);
-                }
-            } catch (UserInterruptException e) {
-                continue;
-            } catch (Exception e) {
-                logger.error(e);
-            }
-        }
+    public CommandHandler(Flogger logger) {
+        this.logger = logger;
+        this.executorService = Executors.newFixedThreadPool(2); // Create a thread pool with 2 threads
     }
 
     public void initialize() {
@@ -78,13 +41,60 @@ public class CommandHandler extends Thread {
         registerCommand(new Help());
         registerCommand(new TestRoute());
 
-        CommandHandler commandHandlerThread = new CommandHandler(logger);
-        commandHandlerThread.setName("CommandHandler");
-        commandHandlerThread.start();
+        executorService.execute(this::readUserInput);
     }
+
+    private void readUserInput() {
+        Terminal terminal;
+        try {
+            terminal = TerminalBuilder.builder().system(true).build();
+            LineReader reader = LineReaderBuilder.builder()
+                            .history(new DefaultHistory())
+                            .terminal(terminal)
+                            .build();
+
+            logger.log(Prefix.INFO, "CommandHandler v2.1 | Type help", 1);
+
+            while (true) {
+                try {
+                    String input = reader.readLine("admin@commonsmh:~$ ");
+
+                    if (input == null)
+                        break;
+
+                    String[] argsCmd = input.split(" ");
+
+                    Boolean foundCmd = false;
+                    for (Command cmd : initializedCommands) {
+                        if (argsCmd[0].equalsIgnoreCase(cmd.getName())) {
+                            cmd.executeAction(argsCmd, logger);
+                            foundCmd = true;
+                            break;
+                        }
+                        ((DefaultHistory) reader.getHistory()).add(argsCmd[0]);
+                    }
+
+                    if (!foundCmd) {
+                        logger.log(Prefix.INFO, "-> No Command found | Type help", 1);
+                    }
+                } catch (UserInterruptException e) {
+                    continue;
+                } catch (Exception e) {
+                    logger.error(e);
+                }
+            }
+        } catch (IOException e) {
+            logger.error(e);
+        }
+    }
+
+
 
     public void registerCommand(Command cmd) {
         initializedCommands.add(cmd);
     }
 
+    public void shutdown() {
+        executorService.shutdownNow();
+    }
 }
