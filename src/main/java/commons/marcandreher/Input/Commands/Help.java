2bef0e910e5e701b4321d9b4f910fddc020bfa83
marcandreher <herpersmarcandre3@gmail.com>
2024-05-01 08:01:04 +0200

    ref: help

--- src/main/java/commons/marcandreher/Input/Commands/Help.java
+++ src/main/java/commons/marcandreher/Input/Commands/Help.java
@@ -1,14 +1,13 @@
 package commons.marcandreher.Input.Commands;
 
-import java.util.List;
-
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.impl.DefaultParser;
-import org.jline.terminal.Terminal;
 import org.jline.utils.InfoCmp;
 
+import commons.marcandreher.Commons.Flogger;
 import commons.marcandreher.Input.Command;
+import commons.marcandreher.Input.CommandHandler;
 import commons.marcandreher.Input.ExtendedCommand;
 
 public class Help extends ExtendedCommand {
@@ -28,9 +27,10 @@ public class Help extends ExtendedCommand {
         return "help";
     }
 
-    private static final int PAGE_SIZE = 10;
+    private final int PAGE_SIZE = 10;
 
-    public static void executeAction(List<Command> commands, Terminal terminal) {
+    @Override
+    public void executeAction(String[] args, Flogger logger) {
         try {
             LineReader reader = LineReaderBuilder.builder()
                     .terminal(terminal)
@@ -39,16 +39,16 @@ public class Help extends ExtendedCommand {
                     .parser(new DefaultParser())
                     .build();
 
-            int totalPages = (int) Math.ceil(commands.size() / (double) PAGE_SIZE);
+            int totalPages = (int) Math.ceil(CommandHandler.initializedCommands.size() / (double) PAGE_SIZE);
             int currentPage = 1;
             int startIndex = 0;
 
-            while (startIndex < commands.size()) {
-                int endIndex = Math.min(startIndex + PAGE_SIZE, commands.size());
+            while (startIndex < CommandHandler.initializedCommands.size()) {
+                int endIndex = Math.min(startIndex + PAGE_SIZE, CommandHandler.initializedCommands.size());
                 terminal.puts(InfoCmp.Capability.clear_screen);
                 terminal.writer().println("Page " + currentPage + "/" + totalPages);
                 for (int i = startIndex; i < endIndex; i++) {
-                    Command c = commands.get(i);
+                    Command c = CommandHandler.initializedCommands.get(i);
                     terminal.writer().println(c.getName() + " - " + c.getDescription());
                 }
                 terminal.flush();
